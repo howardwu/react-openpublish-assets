2eafe59a254e4abc06b9eaabb403ee1cd42d3ad7
Howard Wu <howardwu@berkeley.edu>
2015-08-03 13:07:31 -0700

    Added generalized check for address in example.js

--- example/example.js
+++ example/example.js
@@ -2,7 +2,12 @@ var React = require('react');
 
 var OpenPublishAssets = require('../src');
 
+var address =  'mjf6CRReqGSyvbgryjE3fbGjptRRfAL7cg';
+if (window.location.search.split("?address=") && window.location.search.split("?address=")[1]) {
+  address = window.location.search.split("?address=")[1];
+}
+
 React.render(
-  React.createElement(OpenPublishAssets, { address: 'mjf6CRReqGSyvbgryjE3fbGjptRRfAL7cg' }),
+  React.createElement(OpenPublishAssets, { address: address }),
   document.getElementById('example')
 );
